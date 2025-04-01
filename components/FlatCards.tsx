a47949035591a59d3982cfbfca328287ee3f1a68
Rajdeep Sarkar <rajdeepsarkar271@gmail.com>
2025-04-01 21:54:20 +0530

    initial Commit

---
+++ components/FlatCards.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+export default function FlatCards() {
+  return <Text>FlatCards</Text>;
+}
+
+const styles = StyleSheet.create({});
